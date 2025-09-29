f8c9a6c09580e429a321815229aaa184e217b418
Nada Zirari <zirarinada20@gmail.com>
2025-09-29 16:17:57 +0100

    ajouter interface walletservice dans la couche service

--- src/com/wallet/service/WalletService.java
+++ src/com/wallet/service/WalletService.java
@@ -1,5 +1,10 @@
 package com.wallet.service;
 
+import com.wallet.domain.Wallet;
+import java.sql.SQLException;
+
 public interface WalletService {
+	 Wallet createWallet(String type) throws SQLException;
+	    Wallet getWalletById(String id) throws SQLException;
 
 }
