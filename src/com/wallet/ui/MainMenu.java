9029832a13ee9232b63655e76a509097da5bf4c2
Nada Zirari <zirarinada20@gmail.com>
2025-09-29 16:30:36 +0100

    ajouter attributs and constructor necessaire dans la classe main pour la couche ppresentation

--- src/com/wallet/ui/MainMenu.java
+++ src/com/wallet/ui/MainMenu.java
@@ -1,6 +1,32 @@
 package com.wallet.ui;
 
+
+
+import com.wallet.domain.*;
+import com.wallet.repository.*;
+import com.wallet.service.*;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
 public class MainMenu {
+	
+	 private final WalletService walletService;
+	    private final TransactionService transactionService;
+	    private final Scanner scanner;
+
+	    
+	    public MainMenu() {
+	        // Initialisation des repositories et services
+	        WalletRepository walletRepository = new WalletRepositoryImpl();
+	        TransactionRepository transactionRepository = new TransactionRepositoryImpl();
+
+	        this.walletService = new WalletServiceImpl(walletRepository);
+	        this.transactionService = new TransactionServiceImpl(transactionRepository);
+
+	        this.scanner = new Scanner(System.in);
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
