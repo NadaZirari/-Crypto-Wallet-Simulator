b9237b6b2e366ae5ad342f2fe79058ed9c560790
Nada Zirari <zirarinada20@gmail.com>
2025-09-29 14:41:53 +0100

    ajouter les attributs nessesaire avec le constructeur du classe transaction dans la couche metier

--- src/com/wallet/domain/Transaction.java
+++ src/com/wallet/domain/Transaction.java
@@ -1,5 +1,44 @@
 package com.wallet.domain;
 
+import com.wallet.utils.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Transaction {
+	
+	private String id;
+    private String fromAddress;
+    private String toAddress;
+    private double amount;
+    private FeeLevel feeLevel;
+    private double fee;
+    private TransactionStatus status;
+    private LocalDateTime creationDate;
+    private CryptoType cryptoType;
+
+    public Transaction(String from, String to, double amount, FeeLevel feeLevel, CryptoType type) {
+        this.id = UUID.randomUUID().toString();
+        this.fromAddress = from;
+        this.toAddress = to;
+        this.amount = amount;
+        this.feeLevel = feeLevel;
+        this.cryptoType = type;
+        this.status = TransactionStatus.PENDING;
+        this.creationDate = LocalDateTime.now();
+        this.fee = calculateFee();
+    }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
